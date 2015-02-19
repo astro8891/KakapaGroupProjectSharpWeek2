6fc48fa0730da275da152763a1b6ae4f2f37f4ec
SandraMuller <mursan03@gmail.com>
2015-02-20 11:37:13 +1300

    Project File

---
+++ MathFlash/MathFlash/PlayMathFlash.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFlash
+{
+    class PlayMathFlash
+    {
+        ViewMathFlash view = new ViewMathFlash();
+        List<MathFlash> _FlashMath = new List<MathFlash>();
+
+        public void Play()
+        {
+
+        }
+    }
+}
