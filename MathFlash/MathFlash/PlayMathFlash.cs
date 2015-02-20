6dc437baaa5a581c3c2101532def0e26bef7f54d
SandraMuller <mursan03@gmail.com>
2015-02-20 14:35:52 +1300

    Playing Game

--- MathFlash/MathFlash/PlayMathFlash.cs
+++ MathFlash/MathFlash/PlayMathFlash.cs
@@ -8,19 +8,58 @@ namespace MathFlash
 {
     class PlayMathFlash
     {
+        Random rnd = new Random();
         ViewMathFlash view = new ViewMathFlash();
         List<MathFlash> _MathFlash = new List<MathFlash>();
         string Operator;
+        string UserSays;
+        int number1;
+        int number2;
+        int Answer;
+        int Attempts;
 
         public void Run()
         {
             Operator = view.GetOperator();
             if (Operator != "+" && Operator != "-") { Operator = view.GetOperator(); }
-
+            AddToList();
+            GetAnswer();
+            while (UserSays != "")
+            {
+                UserSays = view.UserPlay(Operator, number1, number2);
+                while (UserSays != Answer.ToString())
+                {
+                    if (Attempts != 3) { UserSays = view.Incorrect(); Attempts++; }
+                    else { Attempts = 0; goto NextProb; }
+                }
+                view.Correct();
+            NextProb: ;
+                AddToList();
+                GetAnswer();
+            }
         }
         public void AddToList()
         {
-            //_MathFlash.Add(new MathFlash( ));
+            number1 = RandomNum();
+            number2 = RandomNum();
+            _MathFlash.Add(new MathFlash(Operator, number1, number2, Answer));
         }
+        public void GetAnswer()
+        {
+            switch (Operator)
+            {
+                case "+":
+                    Answer = number1 + number2;
+                    break;
+                case "-":
+                    Answer = number1 - number2;
+                    break;
+            }
+        }
+        public int RandomNum()
+        {
+            return rnd.Next(0, 9);
+        }
+
     }
 }
